afe9863e5d871d03c13e9e92aa4ade5cee1f0099
paduraru2009 <ciprian.paduraru2009@gmail.com>
2015-01-14 09:47:33 +0200

    Global jump bar
    (Still need to transform into a prefab...)

---
+++ Proj/Assets/GameJumpScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GUI
+{
+
+public class GameJumpScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void OnJumpTomainMenu()
+	{
+		MenuManager.Instance.OpenNewPage (MenuPages.PAGE_MAIN_MENU, true);
+		Debug.Log ("jumped");
+	}
+
+	public void OnLogout()
+	{
+		MenuManager.Instance.OpenNewPage(MenuPages.PAGE_LOGIN, true);
+	}
+
+	public void OnBack()
+	{
+		MenuManager.Instance.GoBack ();
+	}
+}
+
+
+
+
+} // namespace GUI
